09646da949328c285d1b297e8befbe635b05ebbf
GitHub Action <action@github.com>
2022-06-08 17:54:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/balticbb/decisionflow/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/balticbb/decisionflow/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.balticbb.decisionflow.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.balticbb.decisionflow.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/balticbb/decisionflow/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/balticbb/decisionflow/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.balticbb.decisionflow.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.balticbb.decisionflow.MyBundle
 
 class MyApplicationService {
 
